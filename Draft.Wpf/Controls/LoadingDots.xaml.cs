3c637614a1c6124c8d9ff692ffabe23cb3389772
mefabc24 <fabian.mecke@student.reutlingen-university.de>
2026-04-29 17:31:51 +0200

    Extract loading dots into reusable control

---
+++ Draft.Wpf/Controls/LoadingDots.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace Draft.Controls;
+
+public partial class LoadingDots : UserControl
+{
+    public LoadingDots()
+    {
+        InitializeComponent();
+    }
+}
