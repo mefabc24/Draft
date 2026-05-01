dab01011f40ebd7afe09bf5833d01af012e79983
mefabc24 <fabian.mecke@student.reutlingen-university.de>
2026-05-01 18:16:10 +0200

    feat: implement settings UI components and view models

---
+++ Draft.Wpf/Controls/Settings/SettingFieldCard.xaml.cs
@@ -0,0 +1,40 @@
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Markup;
+
+namespace Draft.Wpf.Controls.Settings;
+
+[ContentProperty(nameof(CardContent))]
+public partial class SettingFieldCard : UserControl
+{
+    public static readonly DependencyProperty TitleProperty =
+        DependencyProperty.Register(
+            nameof(Title),
+            typeof(string),
+            typeof(SettingFieldCard),
+            new PropertyMetadata(string.Empty));
+
+    public static readonly DependencyProperty CardContentProperty =
+        DependencyProperty.Register(
+            nameof(CardContent),
+            typeof(object),
+            typeof(SettingFieldCard),
+            new PropertyMetadata(null));
+
+    public SettingFieldCard()
+    {
+        InitializeComponent();
+    }
+
+    public string Title
+    {
+        get => (string)GetValue(TitleProperty);
+        set => SetValue(TitleProperty, value);
+    }
+
+    public object? CardContent
+    {
+        get => GetValue(CardContentProperty);
+        set => SetValue(CardContentProperty, value);
+    }
+}
