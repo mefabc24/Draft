f5cdaffacb30a047ed270dc47c62c1d6552459d6
mefabc24 <fabian.mecke@student.reutlingen-university.de>
2026-07-08 18:12:44 +0200

    feat: link control bar logo to GitHub

--- Draft.Wpf/Shell/Controls/ControlBar.xaml.cs
+++ Draft.Wpf/Shell/Controls/ControlBar.xaml.cs
@@ -1,11 +1,23 @@
+using System.Diagnostics;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace Draft.Shell.Controls;
 
 public partial class ControlBar : UserControl
 {
+    private const string ProjectUrl = "https://github.com/mefabc24/Draft";
+
     public ControlBar()
     {
         InitializeComponent();
+    }
+
+    private void LogoButton_Click(object sender, RoutedEventArgs e)
+    {
+        Process.Start(new ProcessStartInfo(ProjectUrl)
+        {
+            UseShellExecute = true,
+        });
     }
 }
